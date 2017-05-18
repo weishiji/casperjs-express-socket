ec403d31d4c47f63986b7185024feb2567c2ca34
lxg <lxg1986@gmail.com>
2017-05-18 10:30:32 +0800

    socket io 502

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect(window.location.origin);
 socket.on('start', function (data) {
     document.body.innerHTML = data.msg;
 });
